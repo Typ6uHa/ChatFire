f30f5236ad515ee14ce3ccee9803ff30e0d7ab23
Tony <tynx11@gmail.com>
2018-04-25 10:34:18 +0300

    fINAL

---
+++ MyChat/app/src/main/java/com/fire/tynx/mychat/models/Message.kt
@@ -0,0 +1,12 @@
+package com.fire.tynx.mychat.models
+
+class Message {
+
+    constructor() //empty for firebase
+
+    constructor(messageText: String){
+        text = messageText
+    }
+    var text: String? = null
+    var timestamp: Long = System.currentTimeMillis()
+}
